07f06ea8ab52fbe63a93ac123150b67b8373a4cb
Jeremy Wentworth <jeremy@jeremywentworth.com>
2013-05-14 23:20:32 -0400

    cleanup and docs

---
+++ README.md
@@ -0,0 +1,7 @@
+## Example
+    node slicer beat.wav
+
+## Links
+*  [sox](http://sox.sourceforge.net/sox.html): command line audio tool
+*  [shelljs](https://github.com/arturadib/shelljs/): node shell commands
+*  [node-shuffle](https://github.com/troygoode/node-shuffle/): node array shuffle
--- slicer.js
+++ slicer.js
@@ -1,77 +1,85 @@
-/* 
-  #split audio into many files
-  sox --show-progress LEK_173_AGN_FULL.wav output.wav trim 0 0:0.2 : newfile : restart
-
-  #merge all those back together
-  sox output*.wav merged.wav
-
-  #change speed
-  sox --show-progress LEK_173_AGN_FULL.wav output.wav speed 10
-
-  #reverse
-  sox --show-progress LEK_173_AGN_FULL.wav output.wav reverse
-
-  #play
-  play merged.wav
-
-  #3 second, 48kHz, audio file containing a sine-wave swept from 300 to 3300 Hz:
-  sox -n output.wav synth 3 sine 300-3300 vol 0.1
-
-  https://github.com/arturadib/shelljs/
-  https://github.com/troygoode/node-shuffle/
- */
-
 var shell = require("shelljs");
 var shuffle = require("shuffle");
-var prefix = "temp_";
-var sliceLength = "00:00.40";
+var tempFilePrefix = "___temp___";
+var playCommand = "open" //using default player because the sox play command sounds like crap
+var sliceLength = 0.15; //format: 00:00.00
 var centRange = 2400;
+var maxDelayMs = 100;
+var repeatFinalAudioTimes = 1;
+var combineList = ["concatenate"]; //sequence, concatenate, mix, mix-power, merge, multiply
+var outputRateString = "rate 44100";
+var verbose = false;
+var percentAltered = 0.20;
 
-var sliceCmd = "sox --show-progress "+process.argv[2]+" "+prefix+".wav trim 0 " + sliceLength + " : newfile : restart";
-console.log("sliceCmd = " + sliceCmd);
-shell.exec(sliceCmd);
+/******************************************************************************** 
+ * Split up the audio file passed in
+ ********************************************************************************/
+shell.rm(tempFilePrefix + "*.wav");
+if(process.argv.length > 2){
+  var fileArg = process.argv[2];
+  for(var i=0;i<4;i++){
+    exec("sox "+(verbose?"--show-progress":"")+" '" + fileArg + "' " + tempFilePrefix + i + ".wav trim 0 " + (sliceLength*(i+1)) + " : newfile : restart");
+  }
+  sliceLength = 0.750
+}
+var inputFiles = shell.ls(tempFilePrefix+"*.wav");
+if(verbose){ console.log("inputFiles = "+inputFiles); }
 
 
-var inputFiles = shell.ls(prefix+"*.wav");
-console.log("inputFiles = "+inputFiles);
+/******************************************************************************** 
+ * Alter some of the temp files
+ ********************************************************************************/
+var alterCommands = [];
+alterSlices(function(){ return "reverb"; }, percentAltered);
+alterSlices(function(){ return "echos 0.5 0.5 " + [rnd()*maxDelayMs, rnd(), rnd()*maxDelayMs, rnd(), rnd()*500, rnd()].join(" "); }, percentAltered);
+alterSlices(function(){ return "tempo " + (0.25 + rnd()*10); }, percentAltered);
+alterSlices(function(){ return "speed " + (0.25 + rnd()*10); }, percentAltered);
+alterSlices(function(){ return "pitch " + Math.floor((rnd()*centRange*1)-centRange); }, percentAltered);
+alterSlices(function(){ return "repeat " + Math.floor(rnd()*4+1);}, percentAltered-0.10);
+alterSlices(function(){ return "delay " + (rnd()*1);}, percentAltered-0.15);
+alterSlices(function(){ return "reverse"; }, percentAltered);
+if(alterCommands.length > 0) { exec(alterCommands.join(" && ")); }
 
+
+/******************************************************************************** 
+ * Combine all the temp files into one file
+ ********************************************************************************/
+var shuffledFile = fileArg + "_sliced_" + new Date().getTime() + ".wav";
+var combine = combineList[Math.floor(rnd()*combineList.length)];
+var joinedFiles = shuffle.shuffle({deck:inputFiles}).cards.join(" ");
+exec("sox " + joinedFiles + " " + shuffledFile + " --combine " + combine + " --guard " + outputRateString + " repeat " + repeatFinalAudioTimes);
+shell.rm(tempFilePrefix + "*.wav");
+exec(playCommand + " " + shuffledFile);
+
+
+/******************************************************************************** 
+ * Utils
+ ********************************************************************************/
+function rnd(){
+  return Math.random();
+}
+
+function exec(command){
+  if(verbose){ console.log(command.replace(/&& sox/g,"&& \nsox")); }
+  var output = shell.exec(command, {silent:true}).output;
+  if(verbose){ console.log(output); }
+}
 
 function alterSlices(soxEffectCallback, percentOfFilesEffected){
+  if(percentOfFilesEffected <= 0) return;
   var actionDeck = shuffle.shuffle({deck:inputFiles});
   var randomSet = actionDeck.draw(Math.floor(actionDeck.length() * percentOfFilesEffected));
-  //console.log("randomSet = "+randomSet);
 
   if(!(randomSet instanceof Array)){
     randomSet = [randomSet];
   } 
 
   for(var i=0;i<randomSet.length;i++){
-
     var file = randomSet[i];
-    var alterSampleCommand = "sox " + file + " " + prefix + file + " " + soxEffectCallback();
-    console.log("alterSampleCommand = "+alterSampleCommand);
-    shell.exec(alterSampleCommand);
-
-    shell.rm(file);
-    shell.mv(prefix + file, file);
+    var alterSampleCommand = "sox '" + file + "' '" + tempFilePrefix + file + "' " + soxEffectCallback() + 
+                             " && mv '" + tempFilePrefix + file + "' '" + file + "'";
+    alterCommands.push(alterSampleCommand);
   }
 }
-alterSlices(function(){return "pitch " + Math.floor(Math.random()*centRange-centRange);},0.25);
-alterSlices(function(){return "repeat " + Math.floor(Math.random()*4+1);},0.1);
-//alterSlices(function(){return "bend 0," + Math.floor(Math.random()*centRange-centRange) + ",0.19";},0.5);
-alterSlices(function(){return "reverse";},0.25);
-// alterSlices(function(){return "tremelo";},0.25);
-alterSlices(function(){return "reverb";},0.25);
-alterSlices(function(){return "tempo " + (0.25 + Math.random()*10);},0.25);
-
-
-//put back together
-var shuffledFile = "shuffled_"+new Date().getTime()+".wav";
-//var shuffledFile = "shuffled.wav";
-console.log("shuffledFile = "+shuffledFile);
-shell.exec("sox " + shuffle.shuffle({deck:inputFiles}).cards.join(" ") + " " + shuffledFile);
-shell.rm(prefix + "*.wav");
-//shell.exec("play " + shuffledFile);
-shell.exec("open -a Taply.app " + shuffledFile);
 
 
